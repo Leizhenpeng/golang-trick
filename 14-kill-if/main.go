3b02972e7490fb73c0533e8cf16fe9d4fd636eb9
river <laolei@forkway.cn>
2023-02-14 15:06:42 +0800

    feat: add kill if

---
+++ 14-kill-if/main.go
@@ -0,0 +1,31 @@
+package main
+
+type SellInfo struct {
+	Price      float64
+	OrderCount int
+	TotalCount int
+	MemberShip int
+}
+
+func main2() {
+	var a = SellInfo{
+		Price:      1.0,
+		OrderCount: 1,
+		TotalCount: 20,
+		MemberShip: 1,
+	}
+	if a.Price > 0 {
+		println("true")
+	}
+	if a.TotalCount > a.OrderCount {
+		println("true")
+	}
+	if a.MemberShip == 1 {
+		println("true")
+	}
+	if a.Price < 100 && a.MemberShip == 2 {
+		println("true")
+	}
+}
+
+// How To Kill Disgusting If Statement?
---
+++ 14-kill-if/responsibility_chain.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type Rule interface {
+	Check(sellInfo *SellInfo) bool
+}
+
+func Chain(sellInfo *SellInfo, rules ...Rule) bool {
+	for _, r := range rules {
+		if !r.Check(sellInfo) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	sellInfo := &SellInfo{
+		Price:      1.0,
+		OrderCount: 1,
+		TotalCount: 20,
+		MemberShip: 1,
+	}
+
+	rules := []Rule{
+		&PriceRule{},
+		&OrderCountRule{},
+		&MemberShipRule{},
+		&DiscountRule{},
+	}
+	r := Chain(sellInfo, rules...)
+	fmt.Println(r)
+
+}
+
+type PriceRule struct{}
+
+func (pr *PriceRule) Check(sellInfo *SellInfo) bool {
+	return sellInfo.Price > 0
+}
+
+type OrderCountRule struct{}
+
+func (ocr *OrderCountRule) Check(sellInfo *SellInfo) bool {
+	return sellInfo.TotalCount > sellInfo.OrderCount
+}
+
+type MemberShipRule struct{}
+
+func (msr *MemberShipRule) Check(sellInfo *SellInfo) bool {
+	return sellInfo.MemberShip == 1
+}
+
+type DiscountRule struct{}
+
+func (dr *DiscountRule) Check(sellInfo *SellInfo) bool {
+	return sellInfo.Price < 100 && sellInfo.MemberShip == 2
+}
