3a7c512a287aeae7763ce900b8a796d5eae4fea8
river <laolei@forkway.cn>
2023-02-26 08:57:38 +0800

    feat: better error

---
+++ 23-kill-err/killer/nil-style.go
@@ -0,0 +1,70 @@
+package killer
+
+import "fmt"
+
+type Book struct {
+	Name   string
+	Price  int
+	Store  int
+	Member int
+}
+
+func clientExample() {
+	book := &Book{
+		Name:   "golang",
+		Price:  100,
+		Store:  100,
+		Member: 1,
+	}
+	err := book.CalcDiscount(99)
+	if err != nil {
+		return
+	}
+	err = book.JuedeIfStore()
+	if err != nil {
+		return
+	}
+	err = book.IfSale()
+	if err != nil {
+		return
+	}
+	err = book.CalcTotal()
+	if err != nil {
+		return
+	}
+	fmt.Println("ok")
+}
+
+func (b *Book) CalcDiscount(count int) error {
+	// ...
+	b.Price = b.Price * count / 100
+	err := SomeError()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (b *Book) JuedeIfStore() error {
+
+	return nil
+}
+func (b *Book) IfSale() error {
+	// ...
+	return nil
+}
+func (b *Book) CalcTotal() error {
+	// ...
+	return nil
+}
+func (b *Book) AddPay() error {
+	// ...
+	return nil
+}
+func (b *Book) AddOrder() error {
+	// ...
+	return nil
+}
+
+func SomeError() error {
+	return fmt.Errorf("some error")
+}
---
+++ 23-kill-err/killer/struct-style.go
@@ -0,0 +1,82 @@
+package killer
+
+import "fmt"
+
+func someError() error {
+	return fmt.Errorf("some error")
+}
+
+type Book2 struct {
+	Name   string
+	Price  int
+	Store  int
+	Member int
+	error  error
+}
+
+func clientExample2() {
+	book2 := &Book2{
+		Name:   "golang",
+		Price:  100,
+		Store:  100,
+		Member: 1,
+	}
+	book2.
+		CalcDiscount(99).
+		JuedeIfStore().
+		IfSale().
+		AddPay().
+		AddOrder().
+		CheckAll()
+
+	fmt.Println("ok")
+
+}
+
+func (b *Book2) CalcDiscount(count int) *Book2 {
+	if b.error != nil {
+		return b
+	}
+	// 具体的业务逻辑..
+	b.error = someError()
+	return b
+}
+
+func (b *Book2) JuedeIfStore() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	// 具体的业务逻辑
+	return b
+}
+func (b *Book2) IfSale() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	return b
+}
+func (b *Book2) CalcTotal() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	return b
+}
+func (b *Book2) AddPay() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	return b
+}
+func (b *Book2) AddOrder() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	return b
+}
+
+func (b *Book2) CheckAll() *Book2 {
+	if b.error != nil {
+		return b
+	}
+	return b
+}
